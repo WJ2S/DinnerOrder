17f32ba899109de3b7ca5b68f6da45dc9e308945
Shaobo <andyzhang423@gmail.com>
2023-10-07 15:50:03 +0800

    完善实体类

--- src/main/java/com/uts/entity/User.java
+++ src/main/java/com/uts/entity/User.java
@@ -22,4 +22,10 @@ public class User implements Serializable {
     private String username;
     private String email;
     private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
