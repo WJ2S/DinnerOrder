98cd651a9e69f2cd9a6786d48726a74f977c3645
Shaobo <andyzhang423@gmail.com>
2023-10-07 15:10:38 +0800

    增加数据层

---
+++ src/main/java/com/uts/dao/UserMapper.java
@@ -0,0 +1,15 @@
+package com.uts.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.uts.entity.User;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Author: Shaobo
+ * Date: 2023/10/6
+ * Describe:
+ */
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}
